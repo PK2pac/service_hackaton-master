149ad1eadf93e587c0866fdca65cb69f043e740e
natrium14 <natrium744.2@gmail.com>
2017-10-28 16:38:37 +0700

    сделал корочи список мероприятий епты

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,15 @@
+import csv
+from datetime import time, datetime
+import pandas as pd
+
 from django.core import serializers
-from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import *
+from .forms import *
 # Create your views here.
 #import jsonpickle
 
@@ -27,3 +35,25 @@ def chooseCategory(request, id):
     json = serializers.serialize('json', list(contests))
     print(contests)
     return HttpResponse(json, content_type="application/json")
+
+
+@csrf_exempt
+def addData(request):
+    #f = open('./polls/dataset.csv', 'r', encoding='utf-8')
+    data = pd.read_csv('./polls/dataset.csv', delimiter=';', encoding='utf-8')
+    for index, row in data.iterrows():
+        #with open(f) as csvfile:
+        print('sfhsf4h4ht4h4j4h5446jj4j464j')
+        #readCSV = csv.reader(csvfile, delimiter=';')
+        #for row in readCSV:
+            #print(row[0])
+        Contest.objects.create(title=str(row['name']),
+                               description=str(row['description']),
+                               date_start=datetime.strptime(row['start'], "%d.%m.%Y").date(),
+                               date_finish=datetime.strptime(row['finish'], "%d.%m.%Y").date(),
+                               category=int(row['category']),
+                               coordinates=str(row['coordinates']),
+                               site_link=str(row['contacts'])
+                               );
+
+    return redirect("index")
