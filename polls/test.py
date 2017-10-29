091aff389d690f76afbc790ef86e9331b19c1457
natrium14 <natrium744.2@gmail.com>
2017-10-29 13:05:29 +0700

    вроде работает, + бот

---
+++ polls/test.py
@@ -0,0 +1,20 @@
+import polls.constants as constants
+import telebot
+import sqlite3
+from .models import *
+
+types = ['конференции', 'конкурсы', 'выставки и встречи', 'праздники', 'концерты', 'фестивали']
+
+def send_newsletter(event):
+    bot = telebot.TeleBot(constants.token)
+    #conn = sqlite3.connect('./db.sqlite3')
+    #cursor = conn.cursor()
+    #cursor.execute('SELECT * FROM subscribers')
+    #result = cursor.fetchall()
+
+    result = Subsriber.objects.all().values('subscriber_id')
+    message = 'Новое меропритие:\n{}\nНачало: {}\nОкончание: {}\nАдрес: {}\nКонтакты: {}'.format(event[0], event[1], event[2], event[3], event[4])
+
+    for s in result:
+        bot.send_message(s['subscriber_id'], message)
+        bot.send_message(s['subscriber_id'], 'Чтобы отписаться --> /subscribe_off')
